3262ac85478a16ab79c6fff6a25d2b63c9f8ec97
Sofia-Knyshoyid <87498412+Sofia-Knyshoyid@users.noreply.github.com>
2022-10-23 20:26:07 +0300

    checkstyle edit

--- src/test/java/flower/store/FlowerBucketTest.java
+++ src/test/java/flower/store/FlowerBucketTest.java
@@ -6,24 +6,69 @@ import org.junit.jupiter.api.Assertions;
 
 
 public class FlowerBucketTest {
-    private Flower flower = new Flower(2, FlowerColor.BLUE, 30, FlowerType.TULIP);
-    private FlowerPack flowerPack = new FlowerPack(flower, 5);
+    /**
+     * Test variable.
+     */
+    public static final int TESTNUM1 = 2;
+    /**
+     * Test variable.
+     */
+    public static final int TESTNUM2 = 30;
+    /**
+     * Test variable.
+     */
+    public static final int TESTNUM3 = 5;
+    /**
+     * Test variable.
+     */
+    public static final int TESTNUM4 = 3;
+    /**
+     * Test variable.
+     */
+    public static final int TESTNUM5 = 450;
+    /**
+     * The flower variable.
+     */
+    private Flower flower = new Flower(TESTNUM1,
+            FlowerColor.BLUE, TESTNUM2, FlowerType.TULIP);
+    /**
+     * The flower pack variable.
+     */
+    private FlowerPack flowerPack = new FlowerPack(flower, TESTNUM3);
+    /**
+     * The flower bucket variable.
+     */
     private FlowerBucket flowerBucket;
 
+    /**
+     * Creating flower bucket object.
+     */
     @BeforeEach
     public void init() {
-        flowerBucket = new FlowerBucket(flowerPack, 3);
+        flowerBucket = new FlowerBucket(flowerPack, TESTNUM4);
     }
+    /**
+     * Testing flower pack.
+     */
     @Test
     public void testFlowerPack() {
-        Assertions.assertEquals(FlowerType.TULIP, flowerBucket.getFlowerPack().getFlower().getFlowerType());
+        Assertions.assertEquals(FlowerType.TULIP,
+                flowerBucket.getFlowerPack().getFlower().getFlowerType());
     }
+    /**
+     * Testing the amount.
+     */
     @Test
-    public void testAmount() {
-        Assertions.assertEquals(3, flowerBucket.getAmount());
+    public void testAmount()
+    {
+        Assertions.assertEquals(TESTNUM4, flowerBucket.getAmount());
     }
+    /**
+     * Testing the price.
+     */
     @Test
-    public void testPrice() {
-        Assertions.assertEquals(450, flowerBucket.getPrice());
+    public void testPrice()
+    {
+        Assertions.assertEquals(TESTNUM5, flowerBucket.getPrice());
     }
 }
