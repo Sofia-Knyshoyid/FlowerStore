edc3168bc10be6bf439900f1afdfed471766adae
Sofia-Knyshoyid <87498412+Sofia-Knyshoyid@users.noreply.github.com>
2022-10-23 22:33:04 +0300

    add new bucket test

--- src/test/java/flower/store/FlowerBucketTestSecond.java
+++ src/test/java/flower/store/FlowerBucketTestSecond.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Assertions;
 
 
-public class FlowerBucketTest {
+public class FlowerBucketTestSecond {
     /**
      * Test variable.
      */
