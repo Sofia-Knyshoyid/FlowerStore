8ceca5ed72dd9485efe565edfe4f2ef6fdf793d1
Sofia-Knyshoyid <87498412+Sofia-Knyshoyid@users.noreply.github.com>
2022-10-23 18:12:45 +0300

    add getPrice

--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -1,6 +1,32 @@
 package flower.store;
 
+import lombok.Getter;
+
 public class FlowerPack {
+    /**
+     * The type of flower used.
+     */
+    @Getter
     private Flower flower;
-    private int quantity;
+    /**
+     * The amount of flowers.
+     */
+    @Getter
+    private int amount;
+    /**
+     * The price of flower pack.
+     */
+    private int price;
+    FlowerPack(final Flower aFlower, final int theAmount) {
+        flower = aFlower;
+        amount = theAmount;
+        price = amount * (int) flower.getPrice();
+    }
+    /**
+     * Get the price of a flower pack.
+     * @return amount (int).
+     */
+    public int getPrice() {
+        return price;
+    }
 }
