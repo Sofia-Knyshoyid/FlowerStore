8ca042677f4563d740254ab54081f2237b624d97
Sofia-Knyshoyid <87498412+Sofia-Knyshoyid@users.noreply.github.com>
2022-10-23 20:27:18 +0300

    checkstyle edit

--- src/test/java/flower/store/FlowerPackTest.java
+++ src/test/java/flower/store/FlowerPackTest.java
@@ -6,25 +6,60 @@ import org.junit.jupiter.api.Assertions;
 
 
 public class FlowerPackTest {
-    private Flower flower = new Flower(2, FlowerColor.BLUE, 30, FlowerType.TULIP);
+    /**
+     * Test variable.
+     */
+    public static final int TESTNUM1 = 2;
+    /**
+     * Test variable.
+     */
+    public static final int TESTNUM2 = 30;
+    /**
+     * The flower variable.
+     */
+    private Flower flower = new Flower(TESTNUM1, FlowerColor.BLUE,
+            TESTNUM2, FlowerType.TULIP);
+    /**
+     * The flower pack variable.
+     */
     private FlowerPack flowerPack;
 
+    /**
+     * Test variable.
+     */
+    public static final int TESTNUM3 = 5;
+    /**
+     * Setting new object of flower pack.
+     */
     @BeforeEach
     public void init() {
-        flowerPack = new FlowerPack(flower, 5);
+        flowerPack = new FlowerPack(flower, TESTNUM3);
     }
 
-
+    /**
+     * Test result variable.
+     */
+    public static final int TESTNUM4 = 150;
+    /**
+     * The result price of flower pack.
+     */
     @Test
     public void testFlower() {
-        Assertions.assertEquals(FlowerType.TULIP, flowerPack.getFlower().getFlowerType());
+        Assertions.assertEquals(FlowerType.TULIP,
+                flowerPack.getFlower().getFlowerType());
     }
+    /**
+     * The test of amount of flowers in flower pack.
+     */
     @Test
     public void testAmount() {
-        Assertions.assertEquals(5, flowerPack.getAmount());
+        Assertions.assertEquals(TESTNUM3, flowerPack.getAmount());
     }
+    /**
+     * The test of price of the flower pack.
+     */
     @Test
     public void testPrice() {
-        Assertions.assertEquals(150, flowerPack.getPrice());
+        Assertions.assertEquals(TESTNUM4, flowerPack.getPrice());
     }
 }
